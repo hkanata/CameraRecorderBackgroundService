6dd5636b0974782931acc0f6b56db2461af39d00
Miyamoto <hkanata@gmail.com>
2016-05-25 16:32:42 -0300

    Runtime Permissions
    Runtime Permissions

--- app/src/main/java/br/com/opba/MainActivity.java
+++ app/src/main/java/br/com/opba/MainActivity.java
@@ -1,11 +1,15 @@
 package br.com.opba;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -16,13 +20,22 @@ import br.com.tfleet.camerarecorderbackgroundservice.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Request Camera
+    private static final int REQUEST_CAMERA = 1;
+
+    //Request Audio
+    private static final int REQUEST_AUDIO = 2;
+
+    //External Storage
+    private static final int REQUEST_STORAGE = 3;
+
     private Button btnStartRecorder, btnStopRecorder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        askPermissionCamera();
 
         btnStopRecorder = (Button) findViewById(R.id.btnStopRecorder);
         btnStopRecorder.setOnClickListener(new View.OnClickListener() {
@@ -43,8 +56,77 @@ public class MainActivity extends AppCompatActivity {
                 startService(intent);
 
                 //Uncoment this to finish activity
-                finish();
+                //finish();
             }
         });
+    }
+
+    private void askPermissionCamera() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.CAMERA},
+                    REQUEST_CAMERA);
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.RECORD_AUDIO},
+                    REQUEST_AUDIO);
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_STORAGE);
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+            case REQUEST_CAMERA: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    finish();
+                    startActivity(getIntent());
+                } else {
+                    //Denied
+                    Toast.makeText(getApplicationContext(),
+                            "Permission Camera Denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+            case REQUEST_AUDIO: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    finish();
+                    startActivity(getIntent());
+                } else {
+                    //Denied
+                    Toast.makeText(getApplicationContext(),
+                            "Permission Audio Denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+            case REQUEST_STORAGE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    finish();
+                    startActivity(getIntent());
+                } else {
+                    //Denied
+                    Toast.makeText(getApplicationContext(),
+                            "Permission Storage Denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+        }
     }
 }
